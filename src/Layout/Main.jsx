5e8efa647f08d06e973db78aa45333a22ddedca3
Rintia Islam <rintiaislam110@gmail.com>
2024-04-25 21:39:16 +0600

    Navbar Added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Shared/Footer';
+import Navbar from '../Shared/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
